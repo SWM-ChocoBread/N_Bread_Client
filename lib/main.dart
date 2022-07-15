d26665090f018629efd1ed511c7eac9a8bb836d2
chaeeun seo <chaeeunseo@chaeeunui-MacBookPro.local>
2022-07-15 21:47:35 +0900

    [CB-50] make outlinedbutton's style global

--- lib/main.dart
+++ lib/main.dart
@@ -16,22 +16,32 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter 1/N',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
 
-        appBarTheme: const AppBarTheme(
-            backgroundColor: Colors.white, foregroundColor: Colors.black),
-
-        // primaryColor: Colors.white,
-        // primarySwatch: Colors.green,
-      ),
+          appBarTheme: const AppBarTheme(
+              backgroundColor: Colors.white, foregroundColor: Colors.black),
+          outlinedButtonTheme: OutlinedButtonThemeData(
+              style: OutlinedButton.styleFrom(
+            primary: const Color(0xffF6BD60),
+            side: const BorderSide(width: 1.0, color: Color(0xffF6BD60)),
+            textStyle: const TextStyle(
+                // color: Colors.green,
+                fontSize: 16,
+                fontWeight: FontWeight.w300),
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 7),
+            shape: const StadiumBorder(),
+          ))
+          // primaryColor: Colors.white,
+          // primarySwatch: Colors.green,
+          ),
       // initialRoute: Splash.routeName, // 앱에 첫 화면을 지정하는 속성
       // routes: routeSplash, // navigation 화면들을 등록하는 부분
       home: const App(),
