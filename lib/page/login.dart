d8b1be6ac3732369d143dc4bd2fd5fe285a15f1a
chaeeun seo <chaeeunseo@chaeeunui-MacBookPro.local>
2022-07-24 18:00:00 +0900

    [CB-26] make login buttons for naver, kakao and apple

--- lib/page/login.dart
+++ lib/page/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class KakaoLogin extends StatefulWidget {
   KakaoLogin({Key? key}) : super(key: key);
@@ -12,24 +13,106 @@ class _KakaoLoginState extends State<KakaoLogin> {
     return AppBar();
   }
 
+  Widget _naverlogin() {
+    return OutlinedButton(
+      style: OutlinedButton.styleFrom(
+          backgroundColor: const Color(0xff03c75a),
+          side: const BorderSide(width: 1.0, color: Color(0xff03c75a)),
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
+          padding: const EdgeInsets.symmetric(vertical: 13, horizontal: 20)),
+      onPressed: () {},
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SvgPicture.asset(
+            "assets/logo/naver.svg",
+            color: Colors.white,
+          ),
+          const SizedBox(
+            width: 13,
+          ),
+          const Text(
+            "네이버 로그인",
+            style: TextStyle(color: Colors.white),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _kakaologin() {
+    return OutlinedButton(
+      style: OutlinedButton.styleFrom(
+          backgroundColor: const Color(0xffFEE500),
+          side: const BorderSide(width: 1.0, color: Color(0xffFEE500)),
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
+          padding: const EdgeInsets.symmetric(vertical: 13, horizontal: 20)),
+      onPressed: () {},
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset(
+            "assets/logo/kakao.png",
+            color: const Color(0xff000000),
+            width: 18,
+          ),
+          const SizedBox(
+            width: 13,
+          ),
+          Text(
+            "카카오 로그인",
+            style: TextStyle(color: const Color(0xff000000).withOpacity(0.85)),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _applelogin() {
+    return OutlinedButton(
+      style: OutlinedButton.styleFrom(
+          backgroundColor: Colors.black,
+          side: const BorderSide(width: 1.0, color: Colors.black),
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
+          padding: const EdgeInsets.symmetric(vertical: 13, horizontal: 20)),
+      onPressed: () {},
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SvgPicture.asset(
+            "assets/logo/apple.svg",
+            width: 20,
+          ),
+          const SizedBox(
+            width: 10,
+          ),
+          const Text(
+            "Apple로 로그인",
+            style: TextStyle(color: Colors.white),
+          ),
+        ],
+      ),
+    );
+  }
+
   Widget _bodyWidget() {
     return Padding(
       padding: const EdgeInsets.all(15),
       child: Center(
         child: Column(
           children: [
-            OutlinedButton(
-              onPressed: () {},
-              child: const Text("Naver 로 로그인하기"),
+            Container(
+              height: 200,
             ),
-            OutlinedButton(
-              onPressed: () {},
-              child: const Text("Kakao 로 로그인하기"),
-            ),
+            _naverlogin(),
             const SizedBox(
               height: 10,
             ),
-            const Text("여기에 리턴되는 string을 넣어주세요!")
+            _kakaologin(),
+            const SizedBox(
+              height: 10,
+            ),
+            _applelogin(),
           ],
         ),
       ),
