211a235821c428ded50a1893ffb8ba98e4eaa325
chaeeun-seo <seochaeeun961126@gmail.com>
2022-07-31 17:21:16 +0900

    [CB-83] [CB-81] attached firebase analytics and changed theme and arranged colors of the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,21 @@
 import 'package:chocobread/page/app.dart';
 import 'package:chocobread/page/splash/splash.dart';
+import 'package:chocobread/style/colorstyles.dart';
 import 'package:flutter/material.dart';
 import 'package:chocobread/page/routes.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:intl/date_symbol_data_local.dart';
 
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+// void main() {
+//   runApp(const MyApp());
+// }
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
@@ -18,32 +28,39 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter 1/N',
       theme: ThemeData(
-          // This is the theme of your application.
-          //
-          // Try running your application with "flutter run". You'll see the
-          // application has a blue toolbar. Then, without quitting the app, try
-          // changing the primarySwatch below to Colors.green and then invoke
-          // "hot reload" (press "r" in the console where you ran "flutter run",
-          // or simply save your changes to "hot reload" in a Flutter IDE).
-          // Notice that the counter didn't reset back to zero; the application
-          // is not restarted.
-          primaryColor: const Color(0xffF6BD60),
-          appBarTheme: const AppBarTheme(
-              backgroundColor: Colors.white, foregroundColor: Colors.black),
-          outlinedButtonTheme: OutlinedButtonThemeData(
-              style: OutlinedButton.styleFrom(
-            primary: const Color(0xffF6BD60),
-            side: const BorderSide(width: 1.0, color: Color(0xffF6BD60)),
-            textStyle: const TextStyle(
-                // color: Colors.green,
-                fontSize: 16,
-                fontWeight: FontWeight.w300),
-            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 7),
-            shape: const StadiumBorder(),
-          ))
-          // primaryColor: Colors.white,
-          // primarySwatch: Colors.green,
-          ),
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primaryColor: ColorStyle.mainColor,
+        appBarTheme: const AppBarTheme(
+            backgroundColor: Colors.white, foregroundColor: Colors.black),
+        outlinedButtonTheme: OutlinedButtonThemeData(
+            style: OutlinedButton.styleFrom(
+                primary: ColorStyle.mainColor,
+                side: const BorderSide(width: 1.0, color: ColorStyle.mainColor),
+                textStyle: const TextStyle(
+                    // color: Colors.green,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w300),
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                )
+                // const StadiumBorder(),
+                )),
+        colorScheme:
+            ThemeData().colorScheme.copyWith(primary: ColorStyle.mainColor),
+
+        // primaryColor: Colors.white,
+        // primarySwatch: Colors.green,
+      ),
       // initialRoute: Splash.routeName, // 앱에 첫 화면을 지정하는 속성
       // routes: routeSplash, // navigation 화면들을 등록하는 부분
       // 영어로 된 time picker, date picker 한글로 변환
