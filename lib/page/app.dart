757bcb743fb7cc4a1d315f0ee7390125b3b688ba
chaeeun-seo <seochaeeun961126@gmail.com>
2022-07-31 10:47:24 +0900

    [CB-80] [CB-81] change bottom nav bar icons and colors and make it swipe-able using pageview

--- lib/page/app.dart
+++ lib/page/app.dart
@@ -1,7 +1,9 @@
 import 'package:chocobread/page/mypage.dart';
 import 'package:chocobread/page/openchatting.dart';
+import 'package:chocobread/style/colorstyles.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:intl/intl.dart';
 import 'home.dart';
 import 'package:chocobread/constants/sizes_helper.dart';
@@ -19,12 +21,13 @@ class App extends StatefulWidget {
 
 class _AppState extends State<App> {
   late int _currentPageIndex;
+  PageController _pageController = PageController(initialPage: 1);
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    _currentPageIndex = 1;
+    _currentPageIndex = 1; // 처음 페이지는 항상 홈 화면으로 설정
   }
 
   // PreferredSizeWidget _appbarWidget() {
@@ -60,67 +63,89 @@ class _AppState extends State<App> {
   // }
 
   Widget _bodyWidget() {
-    switch (_currentPageIndex) {
-      case 0:
-        return NotionInfo();
-      case 1:
-        return const Home();
-      case 2:
-        return MyPage();
-    }
-    return Container();
+    return PageView(
+      controller: _pageController,
+      onPageChanged: (newPageIndex) {
+        setState(() {
+          _currentPageIndex = newPageIndex;
+        });
+      },
+      children: [NotionInfo(), const Home(), MyPage()],
+    );
+    //   switch (_currentPageIndex) {
+    //     case 0:
+    //       return NotionInfo();
+    //     case 1:
+    //       return const Home();
+    //     case 2:
+    //       return MyPage();
+    //   }
+    //   return Container();
   }
-
-  // BottomNavigationBarItem _bottomNavigationBarItem(String iconName, String label) {
-  //   return BottomNavigationBarItem(icon: Icon(Icons.'${iconName}'), label: label)
-  // }
 
   Widget _bottomNavigationBarWidget() {
     return BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
+        // type: BottomNavigationBarType.fixed,
         onTap: (int index) {
-          print(index);
-          setState(() {
-            _currentPageIndex = index;
-          });
+          _pageController.animateToPage(index,
+              duration: const Duration(milliseconds: 500), curve: Curves.ease);
         },
         elevation: 0,
+        backgroundColor: Colors.transparent,
         currentIndex: _currentPageIndex,
         selectedFontSize: 12,
-        selectedItemColor: Colors.black,
-        selectedLabelStyle: const TextStyle(color: Colors.black),
+        selectedItemColor: ColorStyle.mainColor,
+        selectedLabelStyle: const TextStyle(color: ColorStyle.mainColor),
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
             // icon: SvgPicture.asset("assets/svg/")
             icon: Padding(
               padding: EdgeInsets.only(bottom: 3),
-              child: Icon(Icons.headphones_outlined),
+              child: Icon(
+                Icons.headset_rounded,
+                size: 22,
+              ),
             ),
             activeIcon: Padding(
               padding: EdgeInsets.only(bottom: 3),
-              child: Icon(Icons.headphones),
+              child: Icon(
+                Icons.headset_rounded,
+                size: 24,
+              ),
             ),
             label: "고객센터",
           ),
           BottomNavigationBarItem(
             icon: Padding(
               padding: EdgeInsets.only(bottom: 3),
-              child: Icon(Icons.home_outlined),
+              child: FaIcon(
+                FontAwesomeIcons.house,
+                size: 18,
+              ),
             ),
             activeIcon: Padding(
               padding: EdgeInsets.only(bottom: 3),
-              child: Icon(Icons.home),
+              child: FaIcon(
+                FontAwesomeIcons.house,
+                size: 20,
+              ),
             ),
             label: "홈",
           ),
           BottomNavigationBarItem(
             icon: Padding(
               padding: EdgeInsets.only(bottom: 3),
-              child: Icon(Icons.person_outline),
+              child: FaIcon(
+                FontAwesomeIcons.solidUser,
+                size: 18,
+              ),
             ),
             activeIcon: Padding(
               padding: EdgeInsets.only(bottom: 3),
-              child: Icon(Icons.person),
+              child: FaIcon(
+                FontAwesomeIcons.solidUser,
+                size: 20,
+              ),
             ),
             label: "마이 페이지",
           )
