ae4c5d3f4d7ca19924757d61c959857c3feb6c6b
chaeeun-seo <seochaeeun961126@gmail.com>
2022-08-03 16:57:57 +0900

    [CB-76] deal with images that goes into form

--- lib/page/formchange.dart
+++ lib/page/formchange.dart
@@ -1,10 +1,12 @@
 import 'dart:ffi';
+import 'dart:io';
 
 import 'package:chocobread/constants/sizes_helper.dart';
 import 'package:chocobread/page/customformfield.dart';
 import 'package:chocobread/utils/datetime_utils.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:intl/intl.dart';
 
 import '../style/colorstyles.dart';
@@ -21,11 +23,20 @@ class customFormChange extends StatefulWidget {
 
 class _customFormChangeState extends State<customFormChange> {
   final now = DateTime.now();
+  final ImagePicker imagePickerFromGallery =
+      ImagePicker(); // 갤러리에서 사진 가져오기 위한 것
+  final ImagePicker imagePickerFromCamera = ImagePicker();
+  int? currentnumofimages = 0;
+
+  List<XFile>? imageFileList = []; // 갤러리에서 가져온 사진을 여기에 넣는다.
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
+
+    // imageFileList = widget.data["DealImages"]; //
+
     productNameController.text = widget.data["title"];
     productLinkController.text = widget.data["link"];
     totalPriceController.text =
@@ -81,6 +92,197 @@ class _customFormChangeState extends State<customFormChange> {
 
   final GlobalKey<FormState> _formKey = GlobalKey<
       FormState>(); // added to form widget to identify the state of form
+
+  void selectImagesFromGallery() async {
+    final List<XFile>? selectedImagesFromGallery =
+        await imagePickerFromGallery.pickMultiImage();
+    setState(() {});
+
+    setState(() {
+      imageFileList = []; // 갤러리 버튼을 누를 때마다 이미지 리스트가 비워진다. (새로 다시 선택)
+      if (selectedImagesFromGallery != null) {
+        imageFileList!.addAll(selectedImagesFromGallery);
+      }
+      currentnumofimages = imageFileList?.length;
+    });
+    // imageFileList = []; // 갤러리 버튼을 누를 때마다 이미지 리스트가 비워진다. (새로 다시 선택)
+    // if (selectedImagesFromGallery != null) {
+    //   imageFileList!.addAll(selectedImagesFromGallery);
+    // }
+    // currentnumofimages = imageFileList?.length;
+
+    //else if (selectedImagesFromGallery.isNotEmpty) {
+    //   imageFileList!.addAll(selectedImagesFromGallery);
+    // }
+    setState(() {});
+  }
+
+  void selectImagesFromCamera() async {
+    final XFile? selectedImageFromCamera =
+        await imagePickerFromCamera.pickImage(source: ImageSource.camera);
+    setState(() {});
+    imageFileList = [];
+    if (selectedImageFromCamera != null) {
+      imageFileList!.add(selectedImageFromCamera);
+    }
+  }
+
+  String _getNumberOfImages() {
+    int? numofimages = imageFileList?.length;
+    if (numofimages == 0) {
+      return "0";
+    } else if (numofimages! >= 3) {
+      return "3";
+    }
+    return "${imageFileList?.length}";
+  }
+
+  Duration durationforsnackbar() {
+    int? numofimages = imageFileList?.length;
+    if (numofimages! > 3) {
+      return const Duration(milliseconds: 5000);
+    }
+    return const Duration(milliseconds: 0);
+  }
+
+  Widget _getPhotoButton() {
+    return OutlinedButton(
+        onPressed: () {
+          showModalBottomSheet(
+              shape: const RoundedRectangleBorder(
+                  // modal bottom sheet 의 윗부분을 둥글게 만들어주기
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(30),
+                      topRight: Radius.circular(30))),
+              context: context,
+              builder: (BuildContext context) {
+                return Container(
+                  padding: const EdgeInsets.only(left: 50, right: 50, top: 50),
+                  height: 200,
+                  color: Colors.transparent,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Column(
+                        children: [
+                          OutlinedButton(
+                            onPressed: () {
+                              selectImagesFromGallery();
+                              Navigator.pop(context);
+
+                              const snackBar = SnackBar(
+                                content: Text(
+                                  "사진은 3장까지 업로드할 수 있습니다!",
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                                // backgroundColor: Colors.black,
+                                duration: Duration(milliseconds: 2000),
+                                // behavior: SnackBarBehavior.floating,
+                                elevation: 50,
+                                shape: StadiumBorder(),
+                                // RoundedRectangleBorder(
+                                //     borderRadius: BorderRadius.only(
+                                //         topLeft: Radius.circular(30),
+                                //         topRight: Radius.circular(30))),
+                              );
+                              ScaffoldMessenger.of(context)
+                                  .showSnackBar(snackBar);
+                            }, // 갤러리에서 사진 가져오고
+                            style: OutlinedButton.styleFrom(
+                                padding: const EdgeInsets.all(20),
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(25))),
+                            child: const Icon(
+                              Icons.photo,
+                              size: 30,
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 15,
+                          ),
+                          const Text(
+                            "갤러리",
+                            style: TextStyle(fontSize: 16),
+                          )
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          OutlinedButton(
+                            onPressed: () {
+                              selectImagesFromCamera();
+                            }, // 카메라로 사진 찍기
+                            style: OutlinedButton.styleFrom(
+                                padding: const EdgeInsets.all(20),
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(25))),
+                            child: const Icon(
+                              Icons.camera_alt_rounded,
+                              size: 30,
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 15,
+                          ),
+                          const Text(
+                            "카메라",
+                            style: TextStyle(fontSize: 16),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                );
+              });
+        },
+        style: OutlinedButton.styleFrom(
+            padding: const EdgeInsets.symmetric(horizontal: 22, vertical: 12),
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(25))),
+        child: Column(
+          children: [
+            const Icon(
+              Icons.camera_alt_rounded,
+              size: 30,
+            ),
+            Text("${_getNumberOfImages()}/3") // 0 자리에 사진의 개수가 들어간다.
+          ],
+        ));
+  }
+
+// 3개의 사진이 들어갈 공간
+  final List _boxContents = [Container(), Container(), Container()];
+// 3개만 들어가도록 하는 코드
+  Widget _showPhoto() {
+    return Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 15.0),
+        child: Row(children: [
+          _getPhotoButton(),
+          Flexible(
+            child: GridView.count(
+                crossAxisCount: 3,
+                crossAxisSpacing: 15,
+                padding: const EdgeInsets.all(15),
+                shrinkWrap: true,
+                children: List.generate(
+                  3,
+                  (index) => Container(
+                    decoration: index < imageFileList!.length
+                        ? BoxDecoration(
+                            borderRadius:
+                                const BorderRadius.all(Radius.circular(25)),
+                            color: Colors.grey,
+                            image: DecorationImage(
+                                fit: BoxFit.cover,
+                                image: FileImage(
+                                    File(imageFileList![index].path))))
+                        : null,
+                    child: _boxContents[index],
+                  ),
+                )),
+          )
+        ]));
+  }
 
   Widget _productNameTextFormField() {
     return TextFormField(
@@ -674,6 +876,11 @@ class _customFormChangeState extends State<customFormChange> {
 
   @override
   Widget build(BuildContext context) {
-    return SuggestionFormChange();
+    return Column(
+      children: [
+        _showPhoto(),
+        SuggestionFormChange(),
+      ],
+    );
   }
 }
