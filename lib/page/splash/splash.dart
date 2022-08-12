6192c1e65760c742d51b2e2bbc64f9d854954f07
kite707 <kite707707@gmail.com>
2022-08-12 15:12:20 +0900

    fix kakao login and apple login to use setUserLocation

--- lib/page/splash/splash.dart
+++ lib/page/splash/splash.dart
@@ -19,7 +19,7 @@ class _SplashState extends State<Splash> {
     SharedPreferences prefs = await SharedPreferences
         .getInstance(); // getInstance로 기기 내 shared_prefs 객체를 가져온다.
 
-        //prefs.clear();
+        prefs.clear();
     bool isLogin = prefs.getBool("isLogin") ??
         false; // 처음 앱을 설치했을 때, isLogin 값 자체가 저장되어 있지 않아 null일 것이므로, 이 경우 false로 가져온다.
     bool isTerms = prefs.getBool("isTerms") ?? false; // 약관에 모두 동의했는지 여부
