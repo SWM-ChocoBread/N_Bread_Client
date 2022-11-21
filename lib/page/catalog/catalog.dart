eb89c31830d0eb895f34c815e0d381ee197459c0
chaeeun-seo <seochaeeun961126@gmail.com>
2022-11-21 20:22:42 +0900

    [CB-341] make catalog from dummy data in catalog_repository

---
+++ lib/page/catalog/catalog.dart
@@ -0,0 +1,92 @@
+import 'package:chocobread/page/catalog/catalog_webview.dart';
+import 'package:chocobread/page/repository/catalog_repository.dart';
+import 'package:chocobread/style/colorstyles.dart';
+import 'package:extended_image/extended_image.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class Catalog extends StatefulWidget {
+  const Catalog({super.key});
+
+  @override
+  State<Catalog> createState() => _CatalogState();
+}
+
+class _CatalogState extends State<Catalog> {
+  Widget _imageHolder(int index) {
+    return ClipRRect(
+      borderRadius: const BorderRadius.all(Radius.circular(10)),
+      child: ExtendedImage.network(catalogItems[index]["image_link"].toString(),
+          // width: double.infinity / 2,
+          height: 120,
+          fit: BoxFit.cover,
+          cache: true,
+          enableLoadState: true,
+          retries: 10,
+          timeLimit: const Duration(seconds: 100),
+          timeRetry: const Duration(seconds: 5)),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      itemCount: catalogItems.length,
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2, mainAxisSpacing: 10, crossAxisSpacing: 10),
+      itemBuilder: (BuildContext context, int index) {
+        return GestureDetector(
+          onTap: () async {
+            if (catalogItems[index]["link"] == null) {
+              return null;
+            }
+            if (await canLaunchUrl(Uri.parse(catalogItems[index]["link"]))) {
+              // await launchUrl(
+              //   Uri.parse(catalogItems[index]["link"]),
+              //   // mode: LaunchMode.externalApplication
+              // );
+              Get.to(() => CatalogWebview(url: catalogItems[index]["link"]));
+            }
+          },
+          child: Column(
+            children: [
+              _imageHolder(index),
+              const SizedBox(
+                height: 10,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text(
+                      catalogItems[index]["discount_percent"],
+                      style: const TextStyle(
+                          color: ColorStyle.certificated,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      "${catalogItems[index]["price"]}원",
+                      style: const TextStyle(
+                          fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                child: Row(
+                  children: [Text(catalogItems[index]["unit_price"])],
+                ),
+              )
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
---
+++ lib/page/repository/catalog_repository.dart
@@ -0,0 +1,143 @@
+Map catalogData = {
+  "Items": [
+    {
+      "rate": "5.0",
+      "category": "115673",
+      "unit_price": "(1개당 167원)",
+      "createTime": "1669002258",
+      "deleteTime": 1669034658,
+      "link":
+          "https://www.coupang.com/vp/products/1414288933?itemId=2450844601&vendorItemId=83455514551&sourceType=CATEGORY&categoryId=115573",
+      "price": "14,720",
+      "image_link":
+          "https://thumbnail9.coupangcdn.com/thumbnails/remote/230x230ex/image/retail/images/2022/10/13/11/4/3bb75c37-4655-4842-93b2-569d0273342e.jpg",
+      "name": "순수한면 제로 중형 생리대 날개형, 22개입, 4팩",
+      "discount_percent": "43%",
+      "product_id": "1414288933"
+    },
+    {
+      "rate": "5.0",
+      "category": "115673",
+      "unit_price": "(1개당 186원)",
+      "createTime": "1669002258",
+      "deleteTime": 1669034658,
+      "link":
+          "https://www.coupang.com/vp/products/1414288944?itemId=2450844612&vendorItemId=83455514554&sourceType=CATEGORY&categoryId=115573",
+      "price": "14,900",
+      "image_link":
+          "https://thumbnail10.coupangcdn.com/thumbnails/remote/230x230ex/image/retail/images/2022/10/13/11/2/038a499d-87b5-472f-bb60-bb518749e7b4.jpg",
+      "name": "순수한면 제로 대형 생리대 날개형, 20개입, 4팩",
+      "discount_percent": "42%",
+      "product_id": "1414288944"
+    },
+    {
+      "rate": "5.0",
+      "category": "115673",
+      "unit_price": "(1매당 115원)",
+      "createTime": "1669002258",
+      "deleteTime": 1669034658,
+      "link":
+          "https://www.coupang.com/vp/products/1756477824?itemId=6990259829&vendorItemId=74282560185&sourceType=CATEGORY&categoryId=115573",
+      "price": "11,490",
+      "image_link":
+          "https://thumbnail7.coupangcdn.com/thumbnails/remote/230x230ex/image/retail/images/187063362359519-4e19d0af-c9f0-4dfe-b566-f5d742c6ee1b.jpg",
+      "name": "코멧 일회용 마스크, 100개입, 1개, 화이트",
+      "discount_percent": "58%",
+      "product_id": "1756477824"
+    },
+    {
+      "rate": "5.0",
+      "category": "115673",
+      "unit_price": "(1매당 115원)",
+      "createTime": "1669002258",
+      "deleteTime": 1669034658,
+      "link":
+          "https://www.coupang.com/vp/products/1959587814?itemId=6990259694&vendorItemId=74282560062&sourceType=CATEGORY&categoryId=115573",
+      "price": "11,490",
+      "image_link":
+          "https://thumbnail10.coupangcdn.com/thumbnails/remote/230x230ex/image/retail/images/187050618654682-9bfaeba6-4b57-4050-9022-36290dbb6a33.jpg",
+      "name": "코멧 일회용 마스크, 100개입, 1개, 블랙",
+      "discount_percent": "58%",
+      "product_id": "1959587814"
+    },
+    {
+      "rate": "4.5",
+      "category": "115673",
+      "unit_price": "(10매당 47원)",
+      "createTime": "1669002258",
+      "deleteTime": 1669034658,
+      "link":
+          "https://www.coupang.com/vp/products/247800632?itemId=784574287&vendorItemId=4987386426&sourceType=CATEGORY&categoryId=115573",
+      "price": "14,140",
+      "image_link":
+          "https://thumbnail6.coupangcdn.com/thumbnails/remote/230x230ex/image/retail/images/8939652183402759-0587df8c-303c-449b-b424-68cf7452152d.jpg",
+      "name": "잘풀리는집 맥스 다용도 키친타올 250매, 12개입, 1개",
+      "discount_percent": "21%",
+      "product_id": "247800632"
+    },
+    {
+      "rate": "4.5",
+      "category": "115673",
+      "unit_price": "(10m당 142원)",
+      "createTime": "1669002258",
+      "deleteTime": 1669034658,
+      "link":
+          "https://www.coupang.com/vp/products/261192458?itemId=7633145557&vendorItemId=74924265429&sourceType=CATEGORY&categoryId=115573",
+      "price": "25,590",
+      "image_link":
+          "https://thumbnail10.coupangcdn.com/thumbnails/remote/230x230ex/image/retail/images/1366291716050152-151d7855-a82e-41d6-aa7b-706e6c7cce1a.jpg",
+      "name": "코멧 순백 3겹 라벤더 바닐라 롤화장지 30m, 30롤, 2팩",
+      "discount_percent": "8%",
+      "product_id": "261192458"
+    },
+    {
+      "rate": "4.5",
+      "category": "115673",
+      "unit_price": "(1개당 342원)",
+      "createTime": "1669002258",
+      "deleteTime": 1669034658,
+      "link":
+          "https://www.coupang.com/vp/products/319688139?itemId=3986589394&vendorItemId=71970895995&sourceType=CATEGORY&categoryId=115573",
+      "price": "17,090",
+      "image_link":
+          "https://thumbnail10.coupangcdn.com/thumbnails/remote/230x230ex/image/retail/images/350399385501192-32ff6ef2-b61b-43e0-9580-df4480855eb6.jpg",
+      "name": "탐사 국내 생산 포켓용 핫팩 90g, 50개입",
+      "discount_percent": "56%",
+      "product_id": "319688139"
+    },
+    {
+      "rate": "5.0",
+      "category": "115673",
+      "unit_price": "(1매당 800원)",
+      "createTime": "1669002258",
+      "deleteTime": 1669034658,
+      "link":
+          "https://www.coupang.com/vp/products/6223024351?itemId=12464189891&vendorItemId=79733114235&sourceType=CATEGORY&categoryId=115573",
+      "price": "24,000",
+      "image_link":
+          "https://thumbnail8.coupangcdn.com/thumbnails/remote/230x230ex/image/retail/images/8827394263951471-ce2c0009-9c56-427b-8796-992580cbb673.jpg",
+      "name": "어린숨 엣지 포켓몬스터 아트심볼 컬러 마스크 새부리형 30종 세트 소형, 혼합색상, 30개",
+      "discount_percent": "38%",
+      "product_id": "6223024351"
+    },
+    {
+      "rate": "5.0",
+      "category": "115673",
+      "unit_price": "(100ml당 497원)",
+      "createTime": "1669002258",
+      "deleteTime": 1669034658,
+      "link":
+          "https://www.coupang.com/vp/products/6734632863?itemId=15701794027&vendorItemId=71805128607&sourceType=CATEGORY&categoryId=115573",
+      "price": "15,670",
+      "image_link":
+          "https://thumbnail6.coupangcdn.com/thumbnails/remote/230x230ex/image/retail/images/8221805710373515-fa2c5091-ac03-4bef-a383-79291b18a7f7.jpg",
+      "name": "2020 다우니 엑스퍼트 실내건조 초고농축 섬유유연제 본품, 1.05L, 3개",
+      "discount_percent": "18%",
+      "product_id": "6734632863"
+    }
+  ],
+  "Count": 9,
+  "ScannedCount": 9
+};
+
+List catalogItems = catalogData["Items"];
